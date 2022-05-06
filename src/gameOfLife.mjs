6f076e4c43bb4d9a462dad6d307354f030630bc3
Jan Rundt <jan.rundt@gmail.com>
2022-05-06 11:46:44 +0300

    can read pattern and dimensions

--- src/gameOfLife.mjs
+++ src/gameOfLife.mjs
@@ -1,6 +1,14 @@
+import fs from 'fs';
+
 export function gameOfLife() {
   const file = process.argv[2] || process.env.file ;
-  return file
+  fs.writeFileSync('result.rle', 'something');
+  let data = fs.readFileSync(file).toString();
+  data = data.split('\n');
+  data = data.filter(elem => elem[0] != "#");
+  const dimensions = data[0].split(', ru')[0];
+  const pattern = data[1];
+  return { file: file, dimensions: dimensions, pattern: pattern }
 }
 
 gameOfLife()
--- test/gameOfLife.test.mjs
+++ test/gameOfLife.test.mjs
@@ -1,8 +1,31 @@
 import { expect } from "chai";
 import { gameOfLife } from "../src/gameOfLife.mjs";
+import fs from 'fs';
+import exp from "constants";
 
 describe("Tests for game of life", () => {
-  it("Example test", () => {
-    expect(gameOfLife()).to.equal('blinker.rle');
+  before(() => {
+    if (fs.existsSync('result.rle')) fs.unlinkSync('result.rle');
+  })
+
+  it("Can read file name", () => {
+    const result = gameOfLife();
+    expect(result.file).to.equal('blinker.rle');
   });
+
+  it("Will create a rle-file as a result", () => {
+    gameOfLife();
+    expect(fs.existsSync('result.rle')).to.equal(true);
+  })
+
+  it("Can read dimensions of pattern in file", () => {
+    const result = gameOfLife();
+    expect(result.dimensions).to.equal('x = 3, y = 1');
+  })
+  
+  it("Can read the pattern line from the file", () =>{
+    const result = gameOfLife();
+    expect(result.pattern).to.equal('3o!');
+  })
+
 });
